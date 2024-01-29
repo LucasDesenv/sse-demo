289d94985249803c28be49c1f0b669199fa1c8a0
LucasDesenv <LucasDesenv@github.com>
2024-01-29 14:50:13 +0100

    First commit. Adding few Server Sent Event examples.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Demo for Server Sent Event
+Just a quick demo on how to use SSE with Spring Webflux.
+
+- More about SSE: https://medium.com/deliveryherotechhub/what-is-server-sent-events-sse-and-how-to-implement-it-904938bffd73
+
+@Author: LucasDesenv
---
+++ src/main/java/com/sse/demo/sse/ServerSentEventWebFluxAPI.java
@@ -0,0 +1,36 @@
+package com.sse.demo.sse;
+
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/sse-webflux")
+public class ServerSentEventWebFluxAPI {
+
+  private static List<String> WORDS = Arrays.asList("SSE_1", "SSE_2", "SSE_3", "SSE_4", "SSE_5");
+
+  @GetMapping("/stream")
+  public Flux<ServerSentEvent<String>> wordsStreamEvents() {
+    return Flux.fromStream(WORDS.stream()).map(
+      word -> ServerSentEvent.<String>builder().id(String.valueOf(word)).event("periodic-event")
+        .data("SSE - " + LocalDateTime.now())
+        .build());
+  }
+
+  @GetMapping("/seconds")
+  public Flux<ServerSentEvent<String>> everySecondEvents() {
+    return Flux.interval(Duration.of(1, ChronoUnit.SECONDS))
+      .map(sequence -> ServerSentEvent.<String>builder().id(String.valueOf(sequence)).event("periodic-event")
+        .data("SSE - " + LocalDateTime.now())
+        .build());
+  }
+}
